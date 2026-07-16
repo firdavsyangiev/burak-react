2c630ae8e70b8e8e831a34b6e36d8a3bd5636bcb
Ray <firdavsyangiev@gmail.com>
2026-07-16 09:54:10 +0900

    feat: create Test class component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import ProductsPage from "./screens/productsPage";
@@ -11,6 +11,7 @@ import HelpPage from "./screens/helpPage";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import Test from "./screens/Test";
 
 function App() {
   const location = useLocation();
@@ -32,7 +33,8 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <HomePage />
+          <Test />
+          {/* <HomePage /> */}
         </Route>
       </Switch>
       <Footer />
---
+++ src/app/screens/homePage/Test.tsx
@@ -0,0 +1,51 @@
+// @ts-nocheck
+import React, { Component } from "react";
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964,
+    };
+  }
+  changeDetail = () => {
+    this.setState({
+      color: "blue",
+      brand: "Tesla",
+      model: "Model S",
+      year: 2023,
+    });
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    //runs after render
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    //runs before component unmount
+  }
+
+  componentDidUpdate() {}
+
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          Color: {this.state.color} - Model: {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button type="button" onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Test;
